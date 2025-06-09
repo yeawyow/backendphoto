412897c2f133bf3bbf67af9ff7b45602b7a8645e
yeawyow <yeawyow@gmail.com>
2025-06-09 21:33:07 +0700

    Update worker.py

--- app/worker.py
+++ app/worker.py
@@ -1,10 +1,4 @@
 import os
-# ตั้งค่าก่อน import insightface เพื่อลดการใช้ CPU
-os.environ['OMP_NUM_THREADS'] = '1'
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
-os.environ['MKL_NUM_THREADS'] = '1'
-os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
-os.environ['NUMEXPR_NUM_THREADS'] = '1'
 import aio_pika
 import asyncio
 import json
